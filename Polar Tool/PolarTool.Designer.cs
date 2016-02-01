bb5a48204f5709b82ee01d455a8dcc7530936aa6
jdwoodall <dwoodall@gmail.com>
2016-02-01 07:31:51 -0600

    Intitial Commit - WIP

---
+++ Polar Tool/PolarTool.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Polar_Tool
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.polarChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.polarGrid = new System.Windows.Forms.DataGridView();
+            this.polarMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileStripOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileStripSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileStripExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewStripPolarGraph = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewStripPolarTable = new System.Windows.Forms.ToolStripMenuItem();
+            this.polarGridGroup = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.polarChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polarGrid)).BeginInit();
+            this.polarMenuStrip.SuspendLayout();
+            this.polarGridGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // polarChart
+            // 
+            this.polarChart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            chartArea1.Name = "ChartArea1";
+            this.polarChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.polarChart.Legends.Add(legend1);
+            this.polarChart.Location = new System.Drawing.Point(0, 27);
+            this.polarChart.Name = "polarChart";
+            this.polarChart.Size = new System.Drawing.Size(1002, 510);
+            this.polarChart.TabIndex = 3;
+            title1.Name = "Polar Graph";
+            this.polarChart.Titles.Add(title1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // polarGrid
+            // 
+            this.polarGrid.AllowUserToAddRows = false;
+            this.polarGrid.AllowUserToDeleteRows = false;
+            this.polarGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.polarGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.polarGrid.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.polarGrid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.Format = "N1";
+            dataGridViewCellStyle1.NullValue = "0";
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.MenuText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.polarGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.Format = "N1";
+            dataGridViewCellStyle2.NullValue = "0";
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.polarGrid.DefaultCellStyle = dataGridViewCellStyle2;
+            this.polarGrid.Location = new System.Drawing.Point(0, 19);
+            this.polarGrid.Name = "polarGrid";
+            this.polarGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.Format = "N1";
+            dataGridViewCellStyle3.NullValue = "0";
+            dataGridViewCellStyle3.Padding = new System.Windows.Forms.Padding(3, 0, 3, 0);
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.polarGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.polarGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.MenuText;
+            dataGridViewCellStyle4.Format = "N1";
+            dataGridViewCellStyle4.NullValue = "0";
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.MenuText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.polarGrid.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.polarGrid.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.polarGrid.RowTemplate.DefaultCellStyle.Format = "N1";
+            this.polarGrid.RowTemplate.DefaultCellStyle.NullValue = "0";
+            this.polarGrid.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.polarGrid.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.polarGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.polarGrid.Size = new System.Drawing.Size(1002, 449);
+            this.polarGrid.TabIndex = 4;
+            // 
+            // polarMenuStrip
+            // 
+            this.polarMenuStrip.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.polarMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStrip,
+            this.editToolStrip,
+            this.viewToolStrip});
+            this.polarMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.polarMenuStrip.Name = "polarMenuStrip";
+            this.polarMenuStrip.Size = new System.Drawing.Size(1002, 24);
+            this.polarMenuStrip.TabIndex = 2;
+            this.polarMenuStrip.Text = "polarMenuStrip";
+            // 
+            // fileToolStrip
+            // 
+            this.fileToolStrip.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileStripOpen,
+            this.fileStripSave,
+            this.fileStripExit});
+            this.fileToolStrip.Name = "fileToolStrip";
+            this.fileToolStrip.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStrip.Text = "File";
+            this.fileToolStrip.Click += new System.EventHandler(this.fileToolStrip_Click);
+            // 
+            // fileStripOpen
+            // 
+            this.fileStripOpen.Name = "fileStripOpen";
+            this.fileStripOpen.Size = new System.Drawing.Size(103, 22);
+            this.fileStripOpen.Text = "Open";
+            this.fileStripOpen.Click += new System.EventHandler(this.fileStripOpen_Click);
+            // 
+            // fileStripSave
+            // 
+            this.fileStripSave.Name = "fileStripSave";
+            this.fileStripSave.Size = new System.Drawing.Size(103, 22);
+            this.fileStripSave.Text = "Save";
+            // 
+            // fileStripExit
+            // 
+            this.fileStripExit.Name = "fileStripExit";
+            this.fileStripExit.Size = new System.Drawing.Size(103, 22);
+            this.fileStripExit.Text = "Exit";
+            this.fileStripExit.Click += new System.EventHandler(this.fileStripExit_Click);
+            // 
+            // editToolStrip
+            // 
+            this.editToolStrip.Name = "editToolStrip";
+            this.editToolStrip.Size = new System.Drawing.Size(39, 20);
+            this.editToolStrip.Text = "Edit";
+            // 
+            // viewToolStrip
+            // 
+            this.viewToolStrip.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewStripPolarGraph,
+            this.viewStripPolarTable});
+            this.viewToolStrip.Name = "viewToolStrip";
+            this.viewToolStrip.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStrip.Text = "View";
+            // 
+            // viewStripPolarGraph
+            // 
+            this.viewStripPolarGraph.Name = "viewStripPolarGraph";
+            this.viewStripPolarGraph.Size = new System.Drawing.Size(103, 22);
+            this.viewStripPolarGraph.Text = "Chart";
+            this.viewStripPolarGraph.Click += new System.EventHandler(this.viewStripPolarChart_Click);
+            // 
+            // viewStripPolarTable
+            // 
+            this.viewStripPolarTable.Name = "viewStripPolarTable";
+            this.viewStripPolarTable.Size = new System.Drawing.Size(103, 22);
+            this.viewStripPolarTable.Text = "Table";
+            this.viewStripPolarTable.Click += new System.EventHandler(this.viewStripPolarTable_Click);
+            // 
+            // polarGridGroup
+            // 
+            this.polarGridGroup.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.polarGridGroup.Controls.Add(this.polarGrid);
+            this.polarGridGroup.Location = new System.Drawing.Point(0, 27);
+            this.polarGridGroup.Name = "polarGridGroup";
+            this.polarGridGroup.Size = new System.Drawing.Size(1002, 510);
+            this.polarGridGroup.TabIndex = 5;
+            this.polarGridGroup.TabStop = false;
+            this.polarGridGroup.Text = "Polar TWA/TWS";
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 537);
+            this.Controls.Add(this.polarGridGroup);
+            this.Controls.Add(this.polarMenuStrip);
+            this.Controls.Add(this.polarChart);
+            this.MainMenuStrip = this.polarMenuStrip;
+            this.Name = "formMain";
+            this.Text = "Polar Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.polarChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.polarGrid)).EndInit();
+            this.polarMenuStrip.ResumeLayout(false);
+            this.polarMenuStrip.PerformLayout();
+            this.polarGridGroup.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //
+        // Dialogs and Graphs
+        //
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView polarGrid;
+        private System.Windows.Forms.DataVisualization.Charting.Chart polarChart;
+
+        //
+        // Menu bar
+        //
+        private System.Windows.Forms.MenuStrip polarMenuStrip;
+        //
+        // File Menu
+        //
+        private System.Windows.Forms.ToolStripMenuItem fileToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileStripOpen;
+        private System.Windows.Forms.ToolStripMenuItem fileStripSave;
+        private System.Windows.Forms.ToolStripMenuItem fileStripExit;
+        //
+        // View menu
+        //
+        private System.Windows.Forms.ToolStripMenuItem viewToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem viewStripPolarGraph;
+        private System.Windows.Forms.ToolStripMenuItem viewStripPolarTable;
+        //
+        //Edit menu
+        //
+        private System.Windows.Forms.ToolStripMenuItem editToolStrip;
+        private System.Windows.Forms.GroupBox polarGridGroup;
+    }
+}
+
---
+++ Polar Tool/PolarTool.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Globalization;
+
+namespace Polar_Tool
+{
+    public partial class formMain : Form
+    {
+        // use these for the  size of  the CSV array.  They need to be global to this space.
+        // it has to big enough for any possible array.  It could probably be half this size.
+        const int NumRows = 360;
+        const int NumCols = 100;
+
+        //want this tool to stay in scope although it could be moved into File->Open
+        CSVTools.CSVTools csvt = new CSVTools.CSVTools();
+
+        //want this data to stay in scope
+        string[,] polardata = new string[NumRows, NumCols];
+
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void polarGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void fileToolStrip_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //
+        // View -> Table
+        //
+        // shows the data table
+        private void viewStripPolarTable_Click(object sender, EventArgs e)
+        {
+            polarChart.Hide();
+            polarGridGroup.Show();
+        }
+
+        //
+        // View->Chart
+        //
+        // shows the polar chart
+        private void viewStripPolarChart_Click(object sender, EventArgs e)
+        {
+            polarGridGroup.Hide();
+            polarChart.Show();
+        }
+
+        //
+        //  File -> Open
+        //
+        private void fileStripOpen_Click(object sender, EventArgs e)
+        {
+            string filename;
+
+            openFileDialog1.FileName = "*.csv";
+            openFileDialog1.Filter = "CSV files(*.csv)|*.csv|Text files(*.txt)|*.txt| All files(*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filename = openFileDialog1.FileName;
+                this.Text = "Polar Tool: " + filename;
+            }
+            else
+            {
+                // need error handling here
+                filename = "";
+                Close();
+            }
+
+            // this will read the CSV file into a two dimensional array
+            polardata = csvt.CSVRead(filename, NumRows, NumCols);
+
+            // build the Grid and Polar Charts
+            buildGrid(polardata, csvt.actRows, csvt.actCols);
+            buildPolar(polardata, csvt.actRows, csvt.actCols);
+
+            // show the chart view by default
+            polarChart.Hide();
+            polarGridGroup.Show();
+        }
+
+        //
+        //  File -> Eixt
+        //
+        private void fileStripExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        //
+        // Copies the data in the array polardata into a data table and assigns
+        // it to a DataGridView
+        // rows and cols are the size of the data
+        //
+        private void buildGrid(string[,] polardata, int rows, int cols)
+        {
+            int rowstart;
+            int rowcount, colcount;
+            DataTable dt = new DataTable();
+            Boolean ignoreFirstRow = false;
+            double dnumber;
+            int inumber;
+
+            // We use a DataTable to populate the DataGrid, so build the table
+            // create columns
+            for (colcount = 0; colcount < cols; colcount++)
+            {
+                dt.Columns.Add();
+            }
+
+            // allows to skip first row which is often comments --  Needs to be fixed
+            if (ignoreFirstRow)
+            {
+                rowstart = 1;
+            }
+            else
+            {
+                rowstart = 0;
+            }
+
+            // copy the data from the polardata array in to the data table, row by row
+            for (rowcount = rowstart; rowcount < rows; rowcount++)
+            {
+                // create a DataRow using .NewRow()
+                DataRow row = dt.NewRow();
+
+                // iterate over all columns to fill the row
+                for (colcount = 0; colcount < cols; colcount++)
+                {
+                    try
+                    {
+                        // limit to one decimal point...who needs data in hundreths?
+                        dnumber = Convert.ToDouble(polardata[rowcount, colcount]);
+                        inumber = Convert.ToInt32(dnumber * 10.0);
+                        dnumber = inumber / 10.0;
+                        row[colcount] = dnumber;
+                    }
+                    // the data was not convertable to a decimal number, ie it was text
+                    catch(FormatException e)
+                    {
+                        row[colcount] = polardata[rowcount, colcount];
+                    }
+                }
+
+                // add the current row to the DataTable
+                dt.Rows.Add(row);
+            }
+
+            // Copy the column labels from the first row to the column labels
+            for (colcount = 0; colcount < cols; colcount++)
+            {
+                DataColumn col = dt.Columns[colcount];
+                col.ColumnName = dt.Rows[rowstart][colcount].ToString();
+            }
+
+            // delete the row we just copied the labels from
+            dt.Rows.RemoveAt(0);
+
+            // set the heading of the upper left block
+            DataColumn col0 = dt.Columns[0];
+            col0.ColumnName = "TWA/TWS";
+
+            // Assign to grid view.  Note this method will NOT work with a polar chart (at least that I can figure).
+            polarGrid.DataSource = dt;
+
+            // Not the next two things have to be done AFTER the data table is assigned to the Grid View
+            // so the GridView will have the proper dimensions.
+
+            //Copy Row labels from column 0.  The "-1" is because we deleted a row, above.
+            for (rowcount = 0; rowcount < rows-1; rowcount++)
+            {
+                polarGrid.Rows[rowcount].HeaderCell.Value = dt.Rows[rowcount][0];
+                polarGrid.Rows[rowcount].Cells[0].Value = "";
+            }
+
+            // make the data table columns unsortable.
+            for (colcount = 0; colcount < cols; colcount++)
+            {
+                polarGrid.Columns[colcount].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            // make the column headers visible
+            polarGrid.ColumnHeadersVisible = true;
+        }
+
+
+
+        //
+        //  Build the polar chart
+        //
+        private void buildPolar(string[,] polardata, int rows, int cols)
+        {
+            int rowstart;
+            int rowcount, colcount;
+            Boolean ignoreFirstRow = false;
+            double theta;
+            double r;
+
+            // allows to skip first row which is often comments -- TODO get rid of this
+            if (ignoreFirstRow)
+            {
+                rowstart = 1;
+            }
+            else
+            {
+                rowstart = 0;
+            }
+
+            // clear the chart - this has to be done to re-populate it with other data
+            polarChart.Series.Clear();
+
+            // declare and allocate the data series.  There is is one for each maximum col of data.
+            Series[] polarSeries;
+            polarSeries = new Series[cols];
+
+            //  add data to the chart.  The data is organized by series of wind speeds.  Each data point is the angle and boat speed.
+            //  the rows are at a specific wind angle, the column are the boat speed verus wind speed at that angle.  the wind speed is the firt row.
+            //  the angle is the first column
+
+            // the series are labeled by wind speed
+
+            // go ACROSS the data picking up each row which
+            for (colcount = 1; colcount < cols; colcount++)
+            {
+                // add a series to the polar chart.  The first row in each column is the is the wind speed.
+                // Console.WriteLine("Series added: " + polardata[0, colcount]);
+
+                polarSeries[colcount - 1] = polarChart.Series.Add(polardata[0, colcount]);
+                polarSeries[colcount-1].ChartType = SeriesChartType.Polar;
+
+                //  Add the data points.  The series is added as an XY pair where X = the angle and Y = the boat speed
+                for (rowcount = rowstart + 1; rowcount < rows; rowcount++)
+                {
+//                    Console.Write("Row: " + rowcount + ". Column: " + colcount + ". Data: " + polardata[0, colcount]);
+
+                    // explicitly convert the strings to double
+                    theta = Convert.ToDouble(polardata[rowcount, 0]);
+                    r = Convert.ToDouble(polardata[rowcount, colcount]);
+
+                    polarSeries[colcount - 1].Points.AddXY(theta, r);
+//                    Console.WriteLine("Data added: " + theta + ", " + r);
+//                    Console.WriteLine("Series Data: " + polarSeries[colcount-1].Points[rowcount-rowstart-1].XValue + ", " + polarSeries[colcount-1].Points[rowcount-rowstart-1].YValues[0]);
+                }
+            }
+        }
+    }
+}
